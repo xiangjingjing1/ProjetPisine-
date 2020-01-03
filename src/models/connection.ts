97cfda9d29a1554a825c29e0421120e513ecafc4
Unknown <allocompte@hotmail.fr>
2020-01-03 20:10:32 +0100

    Remove comments

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -12,18 +12,6 @@ const sequelize = (() => {
         if(!process.env.DATABASE_URL) {
             process.exit(1);
         }
-        
-        /*const regex = /^postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)$/;
-        let result = regex.exec(process.env.DATABASE_URL);
-        if(result == null) {
-            process.exit(1);
-        }
-
-        let username = result[1];
-        let password = result[2];
-        let host = result[3];
-        let port = result[4];
-        let dbName = result[5];*/
 
         return new Sequelize(process.env.DATABASE_URL, {
             ssl: true,
