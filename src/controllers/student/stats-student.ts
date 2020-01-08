f29c5bef632ad68e780cec4be1be37999f4d5a82
Lucas <lucas.gonthier@icloud.com>
2020-01-08 18:38:52 +0100

    Begin stats

---
+++ src/controllers/student/stats-student.ts
@@ -0,0 +1,11 @@
+import {Request, Response} from "express";
+
+
+
+function get(req:Request, res:Response){
+    res.render('student/student-progress',{
+        name: 'my progress'
+    })
+};
+
+export default {get};
