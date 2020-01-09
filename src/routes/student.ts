172a3e1dcf2e23bd7de3b14d51149633a6783cd9
Timi <ti.mi.34@hotmail.fr>
2020-01-09 19:34:21 +0100

    Edit route for students statistics

--- src/routes/student.ts
+++ src/routes/student.ts
@@ -2,6 +2,7 @@ import { Express, Router } from "express";
 import sessionDisplay from "../controllers/student/session-display";
 import sessionList from "../controllers/student/session-list";
 import {ensureLoggedIn} from "connect-ensure-login";
+import stats from "../controllers/student/stats";
 
 /**
  * Registers routes used for exam sessions.
@@ -14,12 +15,17 @@ function registerRoutes(app: Express) {
      * This route leads to a page which will list all sessions the user can participate or did participate.
      */
     router.get("/sessions", ensureLoggedIn(), sessionList.get);
-    
+
     /**
      * This route leads to page where student can answer to questions.
      */
     router.get("/sessions/:id", ensureLoggedIn(), sessionDisplay.get);
     router.post("/sessions/:id", ensureLoggedIn(), sessionDisplay.post);
+
+    /**
+     * This route leads to page where student can see his statistics.
+     */
+    router.get("/statistics", ensureLoggedIn(),stats.get);
 
     app.use("/student", router);
 }
